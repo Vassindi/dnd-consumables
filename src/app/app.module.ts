08e67198d49310956434008e23f0c77a734739ac
Heather Reynolds <vassindi@gmail.com>
2022-11-07 19:51:37 -0600

    initial upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CharacterComponent } from './character/character.component';
+import { ConsumableComponent } from './consumable/consumable.component';
+import { NavigationComponent } from './shared/navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CharacterComponent,
+    ConsumableComponent,
+    NavigationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
